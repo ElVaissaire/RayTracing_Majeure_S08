77b6472a8e3354fa35787e9842ffb05387a533f3
elvaissaire <eleonore@vaissaire.fr>
2022-03-04 22:27:29 +0100

    Update Ray.h

--- DemoFreeImage/Ray.h
+++ DemoFreeImage/Ray.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Vector3.h"
+#include "CLight.h"
 
 class CSphere;
 
@@ -8,7 +9,8 @@ class Ray
 public:
 	Ray();
 	Ray(Vector3 *origin, Vector3 *direction);
-	bool intersection(CSphere* s);
+	Vector3 *intersection(CSphere* s);
+	bool intersectionLight(CLight* l);
 	Vector3 *getOrigin();
 	Vector3 *getDirection();
 
